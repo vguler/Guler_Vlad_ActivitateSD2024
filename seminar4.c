0e6c3d5a8d207508c886d0b64b8e1fce5bf9e540
vguler <84806854+vguler@users.noreply.github.com>
2024-04-14 12:47:30 +0300

    Update seminar4.c

--- seminar4.c
+++ seminar4.c
@@ -81,7 +81,7 @@ void stergeLocuinta(struct Locuinta** vector, int* dim, int id) {
 
 void dezalocareMatrice(struct Locuinta*** matrice, int* nr_linii, int** nr_coloane) {
     for (int i = 0; i < (*nr_linii); i++) {
-        for(int j = 0; j < (*nr_coloane)[i]; j++) {
+        for (int j = 0; j < (*nr_coloane)[i]; j++) {
             free((*matrice)[i][j].strada);
             (*matrice)[i][j].strada = NULL;
         }
@@ -101,3 +101,93 @@ void citesteMatriceFisier(const char* nume_fisier, struct Locuinta** matrice, in
             char buffer[100];
             char delimitator[] = ",\n";
             while (fgets(buffer, 100, f) != NULL) {
+                char* token;
+                token = strtok(buffer, delimitator);
+                struct Locuinta locuinta;
+                locuinta.id = atoi(token);
+                token = strtok(NULL, delimitator);
+                locuinta.strada = (char*)malloc(strlen(token) + 1);
+                strcpy(locuinta.strada, token);
+                token = strtok(NULL, delimitator);
+                locuinta.numar = atoi(token);
+                int pozitie = locuinta.numar % 2;
+                inserareLocuinta(&(matrice[pozitie]), &(nr_coloane[pozitie]), locuinta);
+            }
+            fclose(f);
+        }
+    }
+}
+
+void afisareMatrice(struct Locuinta** matrice, int nr_linii, int* nr_coloane) {
+    for (int i = 0; i < nr_linii; i++) {
+        for (int j = 0; j < nr_coloane[i]; j++) {
+            afisareLocuinta(matrice[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void afisareIdDePeOParte(int paritate, struct Locuinta** matrice, int nr_linii, int* nr_coloane) {
+    paritate = paritate % 2;
+    for (int j = 0; j < nr_coloane[paritate]; j++) {
+        printf("%d ", matrice[paritate][j].id);
+    }
+}
+
+struct Locuinta cautareLocuintaDupaID(int idCautat, struct Locuinta** matrice, int nr_linii, int* nr_coloane) {
+    for (int i = 0; i < nr_linii; i++) {
+        for (int j = 0; j < nr_coloane[i]; j++) {
+            if (matrice[i][j].id == idCautat) {
+                return matrice[i][j];
+            }
+        }
+    }
+    struct Locuinta copie;
+    copie.id = -1;
+    copie.numar = -1;
+    copie.strada = NULL;
+    return copie;
+}
+
+void schimbareNumarLocuinta(struct Locuinta** matrice, int nrLinii, int* nrColoane, int numarLocuinta) {
+    int pozitie = numarLocuinta % 2;
+    int idLocuinta = -1;
+    int indexInVector = -1;
+    for (int i = 0; i < nrColoane[pozitie]; i++) {
+        if (matrice[pozitie][i].numar == numarLocuinta) {
+            matrice[pozitie][i].numar++;
+            idLocuinta = matrice[pozitie][i].id;
+            indexInVector = i;
+        }
+    }
+    if (idLocuinta != -1) {
+        int index = (numarLocuinta + 1) % 2;
+        inserareLocuinta(&(matrice[index]), &(nrColoane[index]), matrice[pozitie][indexInVector]);
+        stergeLocuinta(&(matrice[pozitie]), &(nrColoane[pozitie]), idLocuinta);
+    }
+}
+
+void main() {
+    int nr_linii;
+    int* nr_coloane;
+    struct Locuinta** matrice;
+    nr_linii = 2;
+    nr_coloane = (int*)malloc(sizeof(int) * nr_linii);
+    matrice = (struct Locuinta**)malloc(sizeof(struct Locuinta*) * nr_linii);
+    for (int i = 0; i < nr_linii; i++) {
+        matrice[i] = NULL;
+        nr_coloane[i] = 0;
+    }
+    citesteMatriceFisier("locuinte.txt", matrice, nr_linii, nr_coloane);
+    afisareMatrice(matrice, nr_linii, nr_coloane);
+    int paritate = 11;
+    printf("Id-urile cu paritate impara a numarului: \n");
+    afisareIdDePeOParte(paritate, matrice, nr_linii, nr_coloane);
+    struct Locuinta locuinta1 = cautareLocuintaDupaID(17, matrice, nr_linii, nr_coloane);
+    printf("\n\n");
+    afisareLocuinta(locuinta1);
+    schimbareNumarLocuinta(matrice, nr_linii, nr_coloane, 5);
+    printf("\n\n dupa mutare\n");
+    afisareMatrice(matrice, nr_linii, nr_coloane);
+    dezalocareMatrice(&matrice, &nr_linii, &nr_coloane);
+}
